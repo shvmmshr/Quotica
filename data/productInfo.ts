f61fa05cd33bea2689e81ee0347048d8c88836d2
Priyabrata Mondal <95865224+PriyabrataMo@users.noreply.github.com>
2025-04-04 20:36:01 +0530

    Pay (#13)
    * fix: dodo
    
    * fix: dodo2

--- data/productInfo.ts
+++ data/productInfo.ts
@@ -1,9 +1,12 @@
 export const productsMap = {
   creditsRecharge: {
     productName: 'Credits Recharge',
-    productId: 'pdt_g5avtCIbCS0ptsn0HMRUV',
+    productId:
+      process.env.NODE_ENV === 'production'
+        ? 'pdt_ydqBMENmnxfPc1Jy6pisx'
+        : 'pdt_g5avtCIbCS0ptsn0HMRUV',
     productType: 'onetime',
-    redirectUrl: '/',
+    redirectUrl: '/chat',
     minProductPrice: '2.00',
   },
 };
