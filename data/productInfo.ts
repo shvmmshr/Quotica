6e642a9a10214d47dcb57f296d7cda5304561446
Priyabrata <priyabrata8558@gmail.com>
2025-04-04 20:34:02 +0530

    fix: dodo

--- data/productInfo.ts
+++ data/productInfo.ts
@@ -1,7 +1,10 @@
 export const productsMap = {
   creditsRecharge: {
     productName: 'Credits Recharge',
-    productId: 'pdt_g5avtCIbCS0ptsn0HMRUV',
+    productId:
+      process.env.NODE_ENV === 'production'
+        ? 'pdt_ydqBMENmnxfPc1Jy6pisx'
+        : 'pdt_g5avtCIbCS0ptsn0HMRUV',
     productType: 'onetime',
     redirectUrl: '/',
     minProductPrice: '2.00',
