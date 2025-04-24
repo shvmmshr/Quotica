0ea6667b41c3a03990519d058c87936883a4b18d
Priyabrata <priyabrata8558@gmail.com>
2025-04-24 23:07:26 +0530

    feat: change

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,13 +1,15 @@
-// app/chat/page.tsx
-import { Metadata } from "next";
-import ChatLayout from "./_components/chatLayout";
-
-export const metadata: Metadata = {
-  title: "Quotify Chat",
-  description: "Chat with the Quotify assistant",
-};
+'use client';
+import ChatLayout from './_components/chatLayout';
+import { RedirectToSignIn, useAuth } from '@clerk/nextjs';
 
 export default function ChatPage() {
+  const { userId } = useAuth();
+
+  // If user is not logged in, redirect to sign-in page
+  if (!userId) {
+    return <RedirectToSignIn />;
+  }
+
   return (
     <div className="h-full">
       <ChatLayout />
