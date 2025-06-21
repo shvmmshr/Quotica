7d8ebf7a146d12099834ec21f68ab5c24070d257
Priyabrata <priyabrata8558@gmail.com>
2025-06-21 10:55:53 +0530

    feat: refactor sitemap generation to return XML response

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,12 +1,10 @@
-import { MetadataRoute } from 'next';
-
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
+export const GET = async (): Promise<Response> => {
+  const sitemap = [
     {
       url: 'https://quotica.fun',
       lastModified: new Date(),
       changeFrequency: 'daily',
-      priority: 1,
+      priority: 1.0,
     },
     {
       url: 'https://quotica.fun/chat',
@@ -15,4 +13,25 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.9,
     },
   ];
-}
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${sitemap
+      .map(
+        (item) => `
+      <url>
+        <loc>${item.url}</loc>
+        <lastmod>${item.lastModified.toISOString()}</lastmod>
+        <changefreq>${item.changeFrequency}</changefreq>
+        <priority>${item.priority}</priority>
+      </url>`
+      )
+      .join('')}
+  </urlset>`;
+
+  return new Response(xml.trim(), {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  });
+};
